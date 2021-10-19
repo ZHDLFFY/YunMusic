e1088f3279a7d48ee2f3ce9eb667306654fa0628
1712533182@qq.com <1712533182@qq.com>
2021-10-19 21:02:39 +0800

    zxc

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastclick from 'fastclick'
+import './assets/css/base'
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
